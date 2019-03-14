642021f13204aa36b60f5df26881c770571c3c5d
Marcin <marcin.kopanski@gmail.com>
2019-03-14 14:32:44 +0000

    Updates

---
+++ precache-manifest.e226f12afc1d9de8a8c9c48b63fd0f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e3bbb2e5d27433c0272",
+    "url": "/taniarascia-tut/static/css/main.a4185246.chunk.css"
+  },
+  {
+    "revision": "7e3bbb2e5d27433c0272",
+    "url": "/taniarascia-tut/static/js/main.5f1f862a.chunk.js"
+  },
+  {
+    "revision": "08cf8100a573279c135f",
+    "url": "/taniarascia-tut/static/js/runtime~main.2ec3c151.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/taniarascia-tut/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "946eb2c83255e88212e90f85227ba3a3",
+    "url": "/taniarascia-tut/index.html"
+  }
+];
